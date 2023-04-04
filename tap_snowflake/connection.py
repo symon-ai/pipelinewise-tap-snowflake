40045547007174337635258a40a2699b82b7b2c2
sunil-varicent <sshrestha@varicent.com>
2023-04-04 15:22:18 -0400

    prefetch thread to 1

--- tap_snowflake/connection.py
+++ tap_snowflake/connection.py
@@ -76,7 +76,7 @@ class SnowflakeConnection:
             database=self.connection_config['dbname'],
             warehouse=self.connection_config['warehouse'],
             client_prefetch_threads=self.connection_config.get(
-                'client_prefetch_threads', 2),
+                'client_prefetch_threads', 1),
             insecure_mode=self.connection_config.get('insecure_mode', False),
             network_timeout=50
             # Use insecure mode to avoid "Failed to get OCSP response" warnings
